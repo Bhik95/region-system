0b192330f8f2f85b03028b2116558b2944be45ce
Bhik95 <frassybhik@gmail.com>
2021-11-15 11:47:32 +0100

    Added Visualization Methods for the RegionSystem

--- Editor/GridVisualization.cs
+++ Editor/GridVisualization.cs
@@ -5,6 +5,7 @@ using Bhik95.Grids;
 using UnityEngine;
 
 using UnityEditor;
+using Random = UnityEngine.Random;
 
 namespace Bhik95.RegionSystem.Editor
 {
@@ -60,6 +61,57 @@ namespace Bhik95.RegionSystem.Editor
                     Handles.Label(worldPosition, gridXZ[gridPosition].ToString());
                 }
             }
+        }
+
+        public static void ShowRegionSystemOcclusion<T>(RegionSystem<T> regionSystem, GridXZCoordinateConverter coordinateConverter, Color trueColor, Color falseColor, float innerAlpha = 0.5f)
+        {
+            Vector3 cubeSize = new Vector3(coordinateConverter.CellSize.x, coordinateConverter.CellSize.y, 0.1f);
+            
+            for (int ix = 0; ix < regionSystem.GridSize.x; ix++)
+            {
+                for (int iy = 0; iy < regionSystem.GridSize.y; iy++)
+                {
+                    Vector2Int gridPosition = new Vector2Int(ix, iy);
+                    Vector2 worldPosition = coordinateConverter.GridCenterToWorld(gridPosition);
+                    Gizmos.color = regionSystem.GetOcclusionValue(gridPosition)
+                        ? trueColor
+                        : falseColor;
+                    Gizmos.DrawWireCube(worldPosition, cubeSize - 0.02f * Vector3.one);
+                    Gizmos.color = Gizmos.color * innerAlpha;
+                    Gizmos.DrawCube(worldPosition, cubeSize);
+                }
+            }
+        }
+        
+        public static void ShowRegions<T>(RegionSystem<T> regionSystem, GridXZCoordinateConverter coordinateConverter, float innerAlpha = 0.5f)
+        {
+            Random.State initialRandomState = Random.state;
+            
+            Vector3 cubeSize = new Vector3(coordinateConverter.CellSize.x, coordinateConverter.CellSize.y, 0.1f);
+            
+            for (int ix = 0; ix < regionSystem.GridSize.x; ix++)
+            {
+                for (int iy = 0; iy < regionSystem.GridSize.y; iy++)
+                {
+                    Vector2Int gridPosition = new Vector2Int(ix, iy);
+                    Vector2 worldPosition = coordinateConverter.GridCenterToWorld(gridPosition);
+                    if (regionSystem.RegionKeys.HasKey(gridPosition))
+                    {
+                        Vector2Int regionKey = regionSystem.RegionKeys.FindKey(gridPosition);
+                        Random.InitState(regionKey.GetHashCode());
+                        Gizmos.color = Color.HSVToRGB(Random.value, 0.8f, 1.0f);
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.black;
+                    }
+                    Gizmos.DrawWireCube(worldPosition, cubeSize - 0.02f * Vector3.one);
+                    Gizmos.color = Gizmos.color * innerAlpha;
+                    Gizmos.DrawCube(worldPosition, cubeSize);
+                }
+            }
+
+            Random.state = initialRandomState;
         }
     }
 }
